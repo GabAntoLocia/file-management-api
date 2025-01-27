cb0cecdc86ef4df888824f8d0e4a840ce3171a4d
Gabriel Locia <locciaa1@gmail.com>
2025-01-27 16:22:06 -0600

    Add auth  module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+    @Post('register')
+    async register(@Body() body: RegisterDto) {
+        return this.authService.register(body.email, body.password);
+    }
+
+    @Post('login')
+    async login(@Body() body: RegisterDto) {
+        return this.authService.validateUser(body.email, body.password);
+    }
+
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return this.usersService.createUser(email, hashedPassword);
+  }
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user.toObject(); // Excluye la contraseña
+      return result;
+    }
+
+    throw new UnauthorizedException('Credenciales inválidas');
+  }
+}
